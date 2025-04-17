a3908592a39283800241d5beb51ef854a9458ec4
OfriG <96420730+OfriG@users.noreply.github.com>
2025-04-17 15:30:16 +0300

    Update CodeBlockPage.jsx

--- client/src/CodeBlockPage.jsx
+++ client/src/CodeBlockPage.jsx
@@ -39,7 +39,7 @@ export default function CodeBlockPage() {
       navigate('/');
     });
 
-    fetch(`http://localhost:5000/api/codeBlock/${id}`)
+    fetch(`https://codepage-2.onrender.com/api/codeBlock/${id}`)
       .then(res => res.json())
       .then(data => setBlockData(data));
 
